1f201e2f4352ca0bb5564c6bdd84d8da7e15e5ba
Jchan <gkswocks123@gmail.com>
2023-03-03 11:28:20 +0900

    Feat : CRUD 구현 완료

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -8,5 +18,32 @@ export class BoardsController {
   @Get('/')
   getAllBoard() {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(
+    // @Body('title') title: string,
+    // @Body('description') description: string,
+    @Body() createBoardDto: CreateBoardDto,
+  ) {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoardById(@Param('id') id: string): void {
+    return this.boardsService.deleteBoardById(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ): void {
+    this.boardsService.updateBoardStatus(id, status);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { Board } from './boards.model';
+import { BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-  private board: Board[] = [];
+  private boards: Board[] = [];
 
   getAllBoards(): Board[] {
-    return this.board;
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(), // id는 유니크해야 함
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return this.boards;
+  }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoardById(id: string): void {
+    this.boards.find((board) => board.id !== id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): void {
+    const board = this.getBoardById(id);
+    board.status = status;
   }
 }
